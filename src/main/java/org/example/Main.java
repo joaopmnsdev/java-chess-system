1853ee1a464aee1fcd4ee2fe40a98fe8113ed52d
João Pedro Marcon <jpmarcon.10@gmail.com>
2025-08-24 20:56:36 -0300

    First class: Position

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.example.boardgame.Position;
+
 public class Main {
     public static void main(String[] args) {
 
+        Position pos = new Position(2,5);
+        System.out.println(pos);
     }
 }
