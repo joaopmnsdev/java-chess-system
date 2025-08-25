09812155b177f048fe73e1d6effbe5a5be4ffe00
João Pedro Marcon <jpmarcon.10@gmail.com>
2025-08-24 21:04:41 -0300

    Starting to implement Board and Piece

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,11 @@
 package org.example;
 
+import org.example.boardgame.Board;
 import org.example.boardgame.Position;
 
 public class Main {
     public static void main(String[] args) {
 
-        Position pos = new Position(2,5);
-        System.out.println(pos);
+        Board board = new Board(8,8);
     }
 }
