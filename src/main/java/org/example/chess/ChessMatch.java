91d29674d2bb24cbf3640030ea03300b36e2678e
João Pedro Marcon <jpmarcon.10@gmail.com>
2025-08-29 20:51:05 -0300

    Captured pieces implement

--- src/main/java/org/example/chess/ChessMatch.java
+++ src/main/java/org/example/chess/ChessMatch.java
@@ -7,13 +7,18 @@ import org.example.chess.pieces.King;
 import org.example.chess.pieces.Rook;
 
 import javax.swing.plaf.ColorUIResource;
+import java.util.ArrayList;
 import java.util.Currency;
+import java.util.List;
 
 public class ChessMatch {
 
     private int turn;
     private Color currentPlayer;
     private Board board;
+
+    private List<Piece> piecesOnTheBoard = new ArrayList<>();
+    private List<Piece> capturedPieces = new ArrayList<>();
 
     public ChessMatch(){
         board = new Board(8,8);
@@ -68,6 +73,10 @@ public class ChessMatch {
         Piece p = board.removePiece(source);
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
+        if (capturedPiece != null){
+            piecesOnTheBoard.remove(capturedPiece);
+            capturedPieces.add(capturedPiece);
+        }
         return capturedPiece;
     }
 
@@ -90,6 +99,8 @@ public class ChessMatch {
 
     private void placeNewPiece(char column, int row, ChessPiece piece){
         board.placePiece(piece,new ChessPosition(column, row).toPosition());
+        piecesOnTheBoard.add(piece);
+
     }
 
     private void initialSetup(){
