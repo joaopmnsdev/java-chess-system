a448fe97973e9a994309e751be0ae4c91bb2df7d
João Pedro Marcon <jpmarcon.10@gmail.com>
2025-08-29 22:30:22 -0300

    Piece move count

--- src/main/java/org/example/chess/ChessMatch.java
+++ src/main/java/org/example/chess/ChessMatch.java
@@ -81,7 +81,7 @@ public class ChessMatch {
             nextTurn();
         }
 
-        nextTurn();
+        //nextTurn();
         return (ChessPiece) capturedPiece;
     }
 
@@ -92,7 +92,8 @@ public class ChessMatch {
     }
 
     private Piece makeMove(Position source, Position target){
-        Piece p = board.removePiece(source);
+        ChessPiece p = (ChessPiece)board.removePiece(source);
+        p.increaseMoveCount();
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
         if (capturedPiece != null){
@@ -103,7 +104,8 @@ public class ChessMatch {
     }
 
     private void undoMove(Position source, Position target, Piece capturedPiece){
-        Piece p = board.removePiece(target);
+        ChessPiece p = (ChessPiece)board.removePiece(target);
+        p.decreaseMoveCount();
         board.placePiece(p, source);
 
         if(capturedPiece != null){
