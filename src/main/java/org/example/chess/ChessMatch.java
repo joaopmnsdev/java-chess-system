0cc152f641f3bd4f7558cfa9c6e55f6385d00dad
João Pedro Marcon <jpmarcon.10@gmail.com>
2025-08-30 12:32:12 -0300

    Fix exeption for promotion

--- src/main/java/org/example/chess/ChessMatch.java
+++ src/main/java/org/example/chess/ChessMatch.java
@@ -4,8 +4,6 @@ import org.example.boardgame.Board;
 import org.example.boardgame.Piece;
 import org.example.boardgame.Position;
 import org.example.chess.pieces.*;
-
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -126,7 +124,7 @@ public class ChessMatch {
             throw new IllegalStateException("There is not piece to be promoted");
         }
         if(!type.equals("B") && !type.equals("N") &&  !type.equals("R") &&   !type.equals("Q")){
-            throw new InvalidParameterException("Invalid type for promotion");
+            return promoted;
         }
 
         Position pos = promoted.getChessPosition().toPosition();
